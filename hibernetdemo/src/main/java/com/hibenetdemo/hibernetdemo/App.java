1cb6dfd02c314ff0194c3c0818adcb743000423a
swastin <swastin95.sahoo@gmail.com>
2017-05-09 21:00:54 +0530

    commited

--- hibernetdemo/src/main/java/com/hibenetdemo/hibernetdemo/App.java
+++ hibernetdemo/src/main/java/com/hibenetdemo/hibernetdemo/App.java
@@ -1,14 +1,33 @@
 package com.hibenetdemo.hibernetdemo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    @SuppressWarnings("unused")
+	public static void main( String[] args )
     {
+    	Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+    	SessionFactory sf=configuration.buildSessionFactory();
+    	Session session=sf.openSession();
+    	Transaction tx=session.beginTransaction();
+    	User user=new User();
+    	user.setId(10);
+    	user.setName("swastin");
+    	user.setEmail("Swastin95.sahoo");
     	
-        System.out.println( "simple git example!" );
+    	user.setPassword("123456789");
+    	session.save(user);
+    	 tx.commit();  
+        session.close();
+    	
+    	
     }
 }
